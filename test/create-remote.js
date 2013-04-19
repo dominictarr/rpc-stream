c9e7dcabb84c0e3fe02421262bcad008150bd50d
Dominic Tarr <dominic.tarr@gmail.com>
2013-04-19 15:40:06 +0100

    refactor tests

---
+++ test/create-remote.js
@@ -0,0 +1,33 @@
+var rpc  = require('../')
+var es   = require('event-stream')
+var test = require('tape')
+
+var obj = {
+  helloThere: function (name, cb) {
+    cb(null, 'Hello, ' + name)
+  }
+}
+
+test('simple', function (t) {
+
+  //second arg=true means stream of raw js objects,
+  //do not stringify/parse.
+  var a = rpc(null, true)
+  b = rpc(null, true)
+
+  a.createLocalCall('echo', function (args, cb) {
+    t.ok(args)
+    cb(null, args)
+  })
+
+
+  var echo = b.createRemoteCall('echo')
+  //a and b are streams. connect them with pipe.
+  b.pipe(a).pipe(b)
+  var r1 = Math.random(), r2 = Math.random()
+  echo(r1, r2, function (err, args) {
+    t.deepEqual(args, [r1, r2])
+    t.end()
+  })
+})
+
